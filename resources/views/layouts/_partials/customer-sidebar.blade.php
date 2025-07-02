1b3b75a09c3cdc6cd88dc8969ef45f580361ebe9
unknown <rachellfazaa@gmail.com>
2025-07-02 17:15:27 +0800

    update: customer-sidebar

--- resources/views/layouts/_partials/customer-sidebar.blade.php
+++ resources/views/layouts/_partials/customer-sidebar.blade.php
@@ -42,7 +42,7 @@
         </svg>
         Voucher Saya
     </a>
-    <a href="{{ route('customer.points') }}"
+    {{-- <a href="{{ route('customer.points') }}"
         class="flex items-center px-4 py-2 rounded-lg font-semibold {{ request()->routeIs('customer.points') ? 'bg-gray-800 text-white' : 'text-gray-600 hover:bg-gray-100' }}">
         <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -50,5 +50,5 @@
             </path>
         </svg>
         Poin Saya
-    </a>
+    </a> --}}
 </nav>
